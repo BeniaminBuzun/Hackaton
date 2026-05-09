e8329316bc7405104eb58d2851728f92125d6f12
gizler <patrykgizler@student.agh.edu.pl>
2026-05-09 12:52:56 +0200

    Pytania i odpowiedzi

---
+++ server/src/main/java/pl/agh/hackathon/answer/Answer.java
@@ -0,0 +1,45 @@
+package pl.agh.hackathon.answer;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pl.agh.hackathon.question.Question;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="Answers")
+@Entity
+public class Answer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id", nullable=false)
+	private long id;
+
+	/*
+	@Column(name="quizId", nullable=false)
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="id")
+	Quiz quiz;
+	 */
+
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="questionId")
+	Question question;
+
+	/*
+	@Column(name="userId", nullable=false)
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="id")
+	User user;
+	 */
+
+	@Column(name="correctValue", nullable=false)
+	int correct;
+
+	@Column(name="answer", nullable=true)
+	int answer;
+}
---
+++ server/src/main/java/pl/agh/hackathon/question/Question.java
@@ -0,0 +1,49 @@
+package pl.agh.hackathon.question;
+
+import jakarta.persistence.*;
+import lombok.*;
+import pl.agh.hackathon.answer.Answer;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="Questions")
+@Entity
+public class Question {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id", nullable=false)
+	private long id;
+
+	@Column(name="question", nullable=false)
+	private String question;
+
+	/*
+	@Column(name="type", nullable=false)
+	@ManyToOne
+	private QuestionTypes type;
+
+	@Column(name="song", nullable=false)
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name= Columns.SONG_ID)
+	private Song song;
+	 */
+
+	@Column(name="correctAnswer", nullable=false)
+	private String answer1;
+
+	@Column(name="incorrectAnswer", nullable=false)
+	private String answer2;
+
+	@Column(name="answer3", nullable=true)
+	private String answer3;
+
+	@Column(name="answer4", nullable=true)
+	private String answer4;
+
+	@OneToMany(fetch=FetchType.LAZY)
+	private List<Answer> answers;
+}
